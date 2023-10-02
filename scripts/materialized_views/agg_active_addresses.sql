7a902e43c82a5aff8480dac338ec3c4110780fd1
kkhhaalil <119089486+Cryptok777@users.noreply.github.com>
2023-10-02 19:44:16 -0400

    optimize agg view

--- scripts/materialized_views/agg_active_addresses.sql
+++ scripts/materialized_views/agg_active_addresses.sql
@@ -2,29 +2,24 @@ SELECT
   cron.schedule(
     '0 * * * *', 
     $$
-      INSERT INTO agg_active_addresses (date, count) 
-      SELECT 
-        DATE_TRUNC(
-          'day', 
-          TO_TIMESTAMP(block_time / 1e3)
-        ) AS date, 
-        COUNT(
-          DISTINCT(address)
-        ) AS count 
-      FROM 
-        tx_id_address_mapping 
-      WHERE 
-        DATE_TRUNC(
-          'day', 
-          TO_TIMESTAMP(block_time / 1e3)
-        ) :: date = now() :: date 
-      GROUP BY 
-        DATE_TRUNC(
-          'day', 
-          TO_TIMESTAMP(block_time / 1e3)
-        ) ON CONFLICT (date) DO 
-      UPDATE 
-      SET 
-        count = EXCLUDED.count;
+      WITH range AS
+      (
+        SELECT  (EXTRACT(EPOCH
+        FROM CURRENT_DATE::timestamp) * 1e3) :: bigint AS START_DATE, (EXTRACT(EPOCH
+        FROM (CURRENT_DATE + 1)::timestamp) * 1e3) :: bigint AS END_DATE
+      )
+      INSERT INTO agg_active_addresses (date, count)
+      SELECT  DATE_TRUNC('day',TO_TIMESTAMP(block_time / 1e3)) AS date
+            ,COUNT(DISTINCT(address))                         AS count
+      FROM tx_id_address_mapping
+      WHERE block_time >= (
+      SELECT  START_DATE
+      FROM range) AND block_time < (
+      SELECT  END_DATE
+      FROM range)
+      GROUP BY  date
+      ON CONFLICT (date) DO UPDATE
+
+      SET count = EXCLUDED.count;
     $$
 );
